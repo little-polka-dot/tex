2b6ea4dfcc798c38c7a61e4668b092af6a444f43
CatAtHome <44431347+CatAtHome@users.noreply.github.com>
2022-01-15 13:56:10 +0800

    ...

--- tex/models/structure/decoder.py
+++ tex/models/structure/decoder.py
@@ -30,8 +30,8 @@ class Decoder(nn.Module):
 
     def decode(self, dec_input, enc_value, mask=None):
         slf_m = attention.sub_mask(dec_input, self.pad_idx)
-        out_x = self.norm(self.dropout(self.pos_enc(
-            self.embedding(dec_input) * (self.d_model ** 0.5))))
+        out_x = self.norm(self.dropout(
+            self.pos_enc(self.embedding(dec_input) * (self.d_model ** 0.5))))
         for layer in self.h_decoders:
             out_x = layer(out_x, enc_value, slf_mask=slf_m, enc_mask=mask)
         cls_x, box_x = out_x, out_x
